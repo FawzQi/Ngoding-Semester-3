f6165cf48795b88c9a1919add5c97cb0e65beae6
FawzQi <ahamdfaiqfawwaz@gmail.com>
2024-09-25 12:37:52 +0700

    brieren

--- gaming/game.cpp
+++ gaming/game.cpp
@@ -1,8 +1,11 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
 
 #include "raylib.h"
 using namespace std;
+
+int BOSS = 1;
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
@@ -217,7 +220,7 @@ class Player : public Character {
         }
 
         if (attack_state) {
-            if (((game_frame - frame) % 10) == 0)
+            if (((game_frame - frame) % 7) == 0)
                 img_frame++;
             if ((img_frame - 3) == att_frame) {
                 attack_state = false;
@@ -231,7 +234,7 @@ class Player : public Character {
             }
         } else {
             if (stamina <= 100 && stamina > 0) stamina += 0.25;
-            if (stamina < 0) stamina += 0.1;
+            if (stamina <= 0) stamina += 0.15;
 
             if (IsKeyDown(KEY_A) || IsKeyDown(KEY_D)) {
                 img_frame = (game_frame / (IsKeyDown(KEY_SPACE) ? 6 : 6)) % 3 + 1;
@@ -241,14 +244,35 @@ class Player : public Character {
             } else {
                 // DrawTexturePro(img[5], {0, 0, (float)img[5].width, (direction ? 1 : -1) * (float)img[5].height}, {(direction ? 0 : -200) + pos.x, pos.y, (float)img[5].width, (float)img[5].height}, {0, 0}, -90, WHITE);
 
+                // DrawCircleV({(direction ? 315 : -130) + pos.x, pos.y - 95}, 60, RED);
+                // DrawCircleV({(direction ? 315 : -20) + pos.x - 60, pos.y - 120}, 60, GREEN);
+                // DrawCircleV({(direction ? 315 : -20) + pos.x - 60, pos.y - 85}, 60, YELLOW);
+                // DrawCircleV({(direction ? 315 : 110) + pos.x - 120, pos.y - 105}, 100, BLACK);
                 DrawTexturePro(img[0], {0, 0, (float)img[0].width, (direction ? 1 : -1) * (float)img[0].height}, {(direction ? 0 : -200) + pos.x, pos.y, (float)img[0].width, (float)img[0].height}, {0, 0}, -90, WHITE);
             }
         }
     }
+
+    bool attack(Vector2 hitBox, float size_hitbox) {
+        if (img_frame != 8 && img_frame != 5) return false;
+        if (CheckCollisionCircles({(direction ? 315 : -130) + pos.x, pos.y - 95}, 60, hitBox, size_hitbox)) {
+            return true;
+        }
+        if (CheckCollisionCircles({(direction ? 315 : -20) + pos.x, pos.y - 120}, 60, hitBox, size_hitbox)) {
+            return true;
+        }
+        if (CheckCollisionCircles({(direction ? 315 : -20) + pos.x, pos.y - 85}, 60, hitBox, size_hitbox)) {
+            return true;
+        }
+        if (CheckCollisionCircles({(direction ? 315 : 110) + pos.x, pos.y - 105}, 100, hitBox, size_hitbox)) {
+            return true;
+        }
+        return false;
+    }
 };
 Player player;
 
-class Boss_Dog : public Character {
+class Boss_Dog {
    public:
     Vector2 pos = {725, 525};
     float hp = 1000;
@@ -289,31 +313,33 @@ class Boss_Dog : public Character {
         if (game_frame - hit_reset > 300 && player.focus > 0) player.focus -= 0.0366;
         int dmg = 0;
         // DrawCircleV({pos.x + 360, pos.y + 275}, 170, RED);
-        if (player.attack_state && (player.img_frame == 5 || player.img_frame == 8) && (hit_reset + 10) <= game_frame) {
-            if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x, player.pos.y - 85}, 8)) {
-                dmg += 3;
-                player.focus += 4;
-            }
+        if (player.attack({pos.x + 360, pos.y + 275}, 170) && (hit_reset + 10) <= game_frame) {
+            // if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x, player.pos.y - 85}, 8)) {
+            //     dmg += 3;
+            //     player.focus += 4;
+            // }
 
-            if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 20, player.pos.y - 85}, 8)) {
-                dmg += 2;
-                player.focus += 4;
-            }
+            // if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 20, player.pos.y - 85}, 8)) {
+            //     dmg += 2;
+            //     player.focus += 4;
+            // }
 
-            if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 40, player.pos.y - 85}, 8)) {
-                dmg += 2;
-                player.focus += 4;
-            }
+            // if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 40, player.pos.y - 85}, 8)) {
+            //     dmg += 2;
+            //     player.focus += 4;
+            // }
 
-            if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 60, player.pos.y - 85}, 8)) {
-                dmg += 3;
-                player.focus += 4;
-            }
+            // if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 60, player.pos.y - 85}, 8)) {
+            //     dmg += 3;
+            //     player.focus += 4;
+            // }
 
-            if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 80, player.pos.y - 85}, 8)) {
-                dmg += 10;
-                player.focus += 4;
-            }
+            // if (CheckCollisionCircles({pos.x + 360, pos.y + 275}, 170, {(player.direction ? 290 : -186) + player.pos.x + 80, player.pos.y - 85}, 8)) {
+            //     dmg += 10;
+            //     player.focus += 4;
+            // }
+            dmg = 20;
+            player.focus += 16;
             hp -= player.focus == 80 ? dmg * 5 : dmg;
             if (player.focus >= 80) player.focus = 0;
             hit_reset = game_frame;
@@ -341,8 +367,8 @@ class Boss_Dog : public Character {
 
                 if (CheckCollisionCircleRec({(this->direction ? 0 : -55) + bullets.pos.x + 385, bullets.pos.y + 195}, 30, player.hitBox())) {
                     player.hp -= 38;
-                    player.stamina -= 7;
-                    player.pos.x -= (this->direction ? -30 : 30);
+                    if (player.focus > 0) player.focus -= 10;
+                    player.pos.x -= (this->direction ? -50 : 50);
                     bullets.active = false;
                 }
             }
@@ -411,20 +437,21 @@ class Boss_Dog : public Character {
                     att_frame++;
                     if (totat_att - att_frame >= 2) img_frame++;
                     if (totat_att - att_frame < 2) img_frame--;
+
+                    if (img_frame > 5) img_frame = 5;
+
+                    if (att_frame == 3 || att_frame == 8 || att_frame == 13) {
+                        (!direction ? MakeBullet(dogs_bullet, {pos.x - 215, pos.y + 40}, {-35, 0}) : MakeBullet(dogs_bullet, {pos.x + 215, pos.y + 40}, {35, 0}));
+                        if (pos.x + 260 > 0 && pos.x + 460 < 1920) {
+                            direction ? pos.x += -85 : pos.x += 85;
+                        }
+                    }
+                    frame = game_frame;
                     if (totat_att == att_frame) {
                         attack_state = false;
                         punish_frame = 70 * totat_att / 5;
                         att_frame = 0;
                     }
-                    if (img_frame == 8) img_frame = 5;
-
-                    if (att_frame == 3 || att_frame == 8 || att_frame == 13) {
-                        (!direction ? MakeBullet(dogs_bullet, {pos.x - 215, pos.y + 40}, {-35, 0}) : MakeBullet(dogs_bullet, {pos.x + 215, pos.y + 40}, {35, 0}));
-                        if (pos.x + 460 > 0 && pos.x + 460 < 1920) {
-                            direction ? pos.x += -50 : pos.x += 50;
-                        }
-                    }
-                    frame = game_frame;
                 }
             }
 
@@ -454,7 +481,7 @@ class Boss_Dog : public Character {
                 frame = game_frame;
                 img_frame = 3;
                 totat_att = (rand() % 3 + 1) * 5;
-                if (pos.x + 360 <= 0 || pos.x + 460 >= 1920) {
+                if (pos.x + 260 <= 0 || pos.x + 460 >= 1920) {
                     rush_attack = true;
                     att_frame = 45;
                 }
@@ -472,6 +499,81 @@ class Boss_Dog : public Character {
 };
 Boss_Dog boss_dog;
 
+class Boss_Frieren {
+   public:
+    Vector2 pos = {680, 300};
+    float hp = 1000;
+    int frame;
+    int img_frame = 1;
+    int img_count = 1;
+    int hit_reset = 0;
+    Texture2D img[13];
+    bool attack_state = true;
+
+    void initTexture() {
+        img[0] = LoadTexture("Proyek 13_4.png");
+        img[1] = LoadTexture("Proyek 13_5.png");
+        img[2] = LoadTexture("Proyek 13_3.png");
+        img[3] = LoadTexture("Proyek 13_6.png");
+        img[4] = LoadTexture("Proyek 13_7.png");
+        img[5] = LoadTexture("Proyek 13_9.png");
+        img[6] = LoadTexture("Proyek 13_8.png");
+        img[7] = LoadTexture("Proyek 14_2.png");
+        img[8] = LoadTexture("Proyek 14_3.png");
+        img[9] = LoadTexture("Proyek 14_4.png");
+        img[10] = LoadTexture("Proyek 14_5.png");
+        img[11] = LoadTexture("Proyek 14_6.png");
+        img[12] = LoadTexture("Proyek 14_7.png");
+    }
+
+    void unloadTexture() {
+        for (int i = 0; i < 13; i++) {
+            UnloadTexture(img[i]);
+        }
+    }
+
+    void getHit() {
+    }
+
+    void logic() {
+        DrawRectangle(460, 1000, 1000, 30, BLACK);
+        DrawRectangle(460, 1000, hp, 30, RED);
+
+        if (attack_state) {
+            if (game_frame - frame >= 7) {
+                img_frame += img_count;
+                if (img_frame >= 7 || img_frame <= 3) {
+                    img_frame = 4;
+                    img_count *= -1;
+                }
+                frame = game_frame;
+            }
+        } else {
+            if (game_frame - frame >= 7) {
+                img_frame += img_count;
+                if (img_frame >= 3 || img_frame <= -1) {
+                    img_frame = 1;
+                    img_count *= -1;
+                }
+                frame = game_frame;
+            }
+        }
+        pos.y = 300 + sin(game_frame * 0.05) * 30;
+
+        if (player.attack({pos.x + 274, pos.y + 160}, 85) && game_frame - hit_reset >= 12) {
+            hp -= player.focus >= 80 ? 20 * 5 : 20;
+            player.focus += 16;
+            if (player.focus >= 80) player.focus = 0;
+            hit_reset = game_frame;
+        }
+
+        DrawTexturePro(img[img_frame], {0, 0, (float)img[img_frame].width, (float)img[img_frame].height}, {pos.x, pos.y, (float)img[img_frame].width, (float)img[img_frame].height}, {0, 0}, 0, WHITE);
+        DrawTexturePro(img[8], {0, 0, (float)img[8].width, (float)img[8].height}, {pos.x + 224, pos.y + 4, (float)img[8].width, (float)img[8].height}, {0, 0}, 0, WHITE);
+        // DrawCircleV({pos.x + 274, pos.y + 160}, 85, RED);  // hitbox
+    }
+};
+Boss_Frieren boss_frieren;
+
 int main() {
     for (flash& remnants : dash_player) {
         remnants.active = false;
@@ -483,10 +585,12 @@ int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "cool game");
     SetTargetFPS(60);
     ToggleFullscreen();
-
-    Vector2 test_pos = {0, 0};
+    Vector2 test_pos = {906, 308};
+    Vector2 size_test = {100, 100};
     player.initTexture();
     boss_dog.initTexture();
+    boss_frieren.initTexture();
+    Texture2D testimg = boss_frieren.img[8];
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -513,20 +617,38 @@ int main() {
             test_pos.x -= 5;
         }
 
-        // // gambar nilai test_pos dan test_pos2 440 570  570 1015
+        // buat kontrol key ijkl untuk mengatur nilai seize test
+        if (IsKeyDown(KEY_I)) {
+            size_test.y += 5;
+        }
+        if (IsKeyDown(KEY_K)) {
+            size_test.y -= 5;
+        }
+        if (IsKeyDown(KEY_J)) {
+            size_test.x += 5;
+        }
+        if (IsKeyDown(KEY_L)) {
+            size_test.x -= 5;
+        }
+
+        // // gambar nilai test_pos dan size tes 440 570  570 1015
         DrawText(TextFormat("test_pos.x: %f", test_pos.x), 10, 10, 20, RED);
         DrawText(TextFormat("test_pos.y: %f", test_pos.y), 10, 30, 20, RED);
-        if (boss_dog.hp > 0) boss_dog.logic();
+        DrawText(TextFormat("size_test.x: %f", size_test.x), 10, 50, 20, RED);
+        DrawText(TextFormat("size_test.y: %f", size_test.y), 10, 70, 20, RED);
+        if (!BOSS) {
+            if (boss_dog.hp > 0) boss_dog.logic();
+        } else {
+            if (boss_frieren.hp > 0) boss_frieren.logic();
+        }
         player.logic();
-        // if (player.hp <= 0) {
-        //     break;
-        // }
-        //  DrawTexturePro(boss_dog.img[8], {0, 0, (float)boss_dog.img[8].width, (float)boss_dog.img[8].height}, {test_pos.x, test_pos.y, (float)boss_dog.img[8].width, (float)boss_dog.img[8].height}, {0, 0}, 0, WHITE);
+        // DrawTexturePro(testimg, {0, 0, (float)testimg.width, (float)testimg.height}, {test_pos.x, test_pos.y, size_test.x, size_test.y}, {0, 0}, 0, WHITE);
         EndDrawing();
         game_frame++;
     }
     player.unloadTexture();
     boss_dog.unloadTexture();
+    boss_frieren.unloadTexture();
     CloseWindow();
     return 0;
 }
